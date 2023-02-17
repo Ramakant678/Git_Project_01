44795e9c65e49c54947d8c335388a50d7f57de96
Ramakant Kumar <ramakantnmiet123@gmail.com>
2023-02-17 15:24:52 +0530

    Initial Commit

---
+++ src/com/india/echo/India.java
@@ -0,0 +1,21 @@
+package com.india.echo;
+
+public class India {
+	
+	String pm;
+	String pr;
+	public String sayPm() {
+		return pm;
+	}
+	public void setPm(String pm) {
+		this.pm=pm;
+	}
+	public String sayPr() {
+		return pr;
+	}
+	public void setPr(String pr) {
+		this.pr=pr;
+	}
+	
+
+}
---
+++ src/com/india/echo/Test.java
@@ -0,0 +1,15 @@
+package com.india.echo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello This is India Information");
+		India ind=new India();
+		ind.setPm("Narendra Modi");
+		ind.setPr("Murmu");
+		System.out.println(ind.sayPm());
+		System.out.println(ind.sayPr());
+
+	}
+
+}
